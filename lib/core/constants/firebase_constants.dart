2f9282f92afd61ac48642784001665c26c328c70
Kiishi David <user@Kiishis-MacBook-Pro.local>
2023-08-01 09:50:06 +0100

    loaded my tools in

---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,10 @@
+class FirebaseConstants {
+  static const usersCollection = 'users';
+  static const postsCollection = 'posts';
+  static const repostsCollection = 'reposts';
+  static const commentsCollection = 'comments';
+  static const quotesCollection = 'quotes';
+  static const notificationsCollection = 'notifications';
+  static const bookmarksCollection = 'bookmarks';
+  static const verificationCollection = 'verifications';
+}
---
+++ lib/shared/app_grafiks.dart
@@ -0,0 +1,4 @@
+abstract class AppGrafiks {
+  static const String logo = 'logo';
+  static const String google = 'google';
+}
